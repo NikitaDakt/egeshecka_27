83b5db99c37c4c25804b2ef03f96732a71ed4424
Nikita <mr.dakt@mail.ru>
2021-12-18 00:27:59 +0300

    added development plan

--- main.cpp
+++ main.cpp
@@ -1,5 +1,19 @@
 #include <iostream>
 
-int main() {
+using namespace std;
 
+void max_sum_div()
+{
+    /**
+    1)считать и сохранить все числа
+    2) вывести все пары
+    3) вместо пары вывести суммы
+    4) проверить делимость суммы
+    5) вывести только наибольшую сумму
+    */
+}
+int main()
+{
+    cout << "Hello world!";
+    return 0;
 }
