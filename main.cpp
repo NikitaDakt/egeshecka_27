042a72fdd998be7a43398e10758cdeca881ee9ff
Nikita <mr.dakt@mail.ru>
2021-12-22 20:50:43 +0300

    reads and shows numbers with first condition

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+#include <vector>
 
 using namespace std;
 
@@ -11,11 +11,21 @@ void num_sum_max_distance_ineff()
      3) вывести пары удовлетворяющие второму условию
      4) вывести ответ
     */
-}
+    int n;
+    cin >> n;
+    vector<int> numbers(n, 0);
+    for (int i(0); i < n; i++)
+        cin >> numbers[i];
 
+    for (int i = 0; i < n; ++i)
+        for (int j = i + 1; j < n; ++j)
+        {
+            if (abs(i - j) < 10)
+                cout << numbers[i] << ' ' << numbers[j] << endl;
+        }
+}
 int main()
 {
     num_sum_max_distance_ineff();
     return 0;
 }
-
