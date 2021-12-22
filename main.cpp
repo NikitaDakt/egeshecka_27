3963b811098f65a7d9657b28a71bbdd1e576d25a
Nikita <mr.dakt@mail.ru>
2021-12-22 20:49:53 +0300

    Added development plan

--- main.cpp
+++ main.cpp
@@ -1,5 +1,21 @@
 #include <iostream>
 
-int main() {
 
+using namespace std;
+
+void num_sum_max_distance_ineff()
+{
+    /**
+     1)считать последовательность чисел
+     2) вывести пары удовлетворяющие первому условию
+     3) вывести пары удовлетворяющие второму условию
+     4) вывести ответ
+    */
 }
+
+int main()
+{
+    num_sum_max_distance_ineff();
+    return 0;
+}
+
